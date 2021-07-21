339399a671f087af99b49cf5c0050c5d2366f9e7
anatoli <3anatoliageichik@gmail.com>
2021-07-21 12:35:28 +0300

    fixed bugs

--- translator/src/main.js
+++ translator/src/main.js
@@ -4,7 +4,7 @@ import router from "./router/index"
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-//import VueRouter from "vue-router"
+
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue)
